75d384ef2a93645c1b8a66e5045f839427d6de5c
omarfareed23 <omar.abdelaaty02@eng-st.cu.edu.eg>
2023-04-19 14:11:26 +0200

    deserialize world

--- source/common/ecs/world.cpp
+++ source/common/ecs/world.cpp
@@ -9,10 +9,14 @@ namespace our {
         if(!data.is_array()) return;
         for(const auto& entityData : data){
             //TODO: (Req 8) Create an entity, make its parent "parent" and call its deserialize with "entityData".
-            
+            // Then add the new entity to the world's entities list
+            Entity* entity = new Entity();
+            entity->parent = parent;
+            entity->deserialize(entityData);
             if(entityData.contains("children")){
                 //TODO: (Req 8) Recursively call this world's "deserialize" using the children data
                 // and the current entity as the parent
+                this->deserialize(entityData["children"], entity);
                 
             }
         }
