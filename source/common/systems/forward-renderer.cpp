3157b0d441359832539b66139f24cf969a53b2be
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2023-04-22 19:31:07 +0200

    beginning req 9

--- source/common/systems/forward-renderer.cpp
+++ source/common/systems/forward-renderer.cpp
@@ -135,21 +135,41 @@ namespace our {
 
         //TODO: (Req 9) Modify the following line such that "cameraForward" contains a vector pointing the camera forward direction
         // HINT: See how you wrote the CameraComponent::getViewMatrix, it should help you solve this one
-        glm::vec3 cameraForward = glm::vec3(0.0, 0.0, -1.0f);
+        glm::vec3 cameraForward = glm::vec3(camera->getOwner()->getLocalToWorldMatrix() * glm::vec4(0.0, 0.0, -1.0f, 0.0));
         std::sort(transparentCommands.begin(), transparentCommands.end(), [cameraForward](const RenderCommand& first, const RenderCommand& second){
             //TODO: (Req 9) Finish this function
-            // HINT: the following return should return true "first" should be drawn before "second". 
-            return false;
+            // HINT: the following return should return true "first" should be drawn before "second".
+
+            // we calculate dot product of the center point along camera forward direction
+            float firstDistance = first.center.x * cameraForward.x + first.center.y * cameraForward.y + first.center.z * cameraForward.z;
+            float secondDistance = second.center.x * cameraForward.x + second.center.y * cameraForward.y + second.center.z * cameraForward.z;
+            // then we compare the 2 distance and put the farther first
+            return firstDistance > secondDistance;
         });
 
         //TODO: (Req 9) Get the camera ViewProjection matrix and store it in VP
-        
+
+        // first, get view matrix from camera component
+        glm::mat4 cameraViewMatrix = camera->getViewMatrix();
+        // second, get projection matrix from camera component using window size property
+        glm::mat4 cameraProjectionMatrix = camera->getProjectionMatrix(this->windowSize);
+        // third, get VP matrix by multiplying projection * view
+        glm::mat4 VP = cameraProjectionMatrix * cameraViewMatrix;
+
         //TODO: (Req 9) Set the OpenGL viewport using viewportStart and viewportSize
+
+        // starting x, y, and width and height
+        glViewport(0, 0, this->windowSize.x, this->windowSize.y);
         
         //TODO: (Req 9) Set the clear color to black and the clear depth to 1
-        
+        glClearColor(0.0, 0.0, 0.0, 1.0);
+        glClearDepth(1.0);
         //TODO: (Req 9) Set the color mask to true and the depth mask to true (to ensure the glClear will affect the framebuffer)
-        
+
+        // enables/disables writing on the depth buffer (enabled here)
+        glDepthMask(true);
+        // enable/disable writing colors in the frame buffer
+        glColorMask(true, true, true, true);
 
         // If there is a postprocess material, bind the framebuffer
         if(postprocessMaterial){
@@ -158,10 +178,15 @@ namespace our {
         }
 
         //TODO: (Req 9) Clear the color and depth buffers
-        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         //TODO: (Req 9) Draw all the opaque commands
         // Don't forget to set the "transform" uniform to be equal the model-view-projection matrix for each render command
-        
+        for(RenderCommand opaqueCommand: this->opaqueCommands) {
+            GLint transformLoc = opaqueCommand.material->shader->getUniformLocation("transform");
+            glUniformMatrix4fv(transformLoc, 1, false, (float*)&VP);
+            opaqueCommand.mesh->draw();
+        }
         // If there is a sky material, draw the sky
         if(this->skyMaterial){
             //TODO: (Req 10) setup the sky material
@@ -185,7 +210,11 @@ namespace our {
         }
         //TODO: (Req 9) Draw all the transparent commands
         // Don't forget to set the "transform" uniform to be equal the model-view-projection matrix for each render command
-        
+        for(RenderCommand transparentCommand: this->transparentCommands) {
+            GLint transformLoc = transparentCommand.material->shader->getUniformLocation("transform");
+            glUniformMatrix4fv(transformLoc, 1, false, (float*)&VP);
+            transparentCommand.mesh->draw();
+        }
 
         // If there is a postprocess material, apply postprocessing
         if(postprocessMaterial){
