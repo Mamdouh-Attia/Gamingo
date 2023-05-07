3c14a5716dfff2f32e7bf2a7470233ada852c25f
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2023-05-07 22:29:20 +0200

    finished obstacle component

---
+++ source/common/components/obstacle.cpp
@@ -0,0 +1,11 @@
+#include "obstacle.hpp"
+#include "../ecs/entity.hpp"
+#include "../deserialize-utils.hpp"
+
+namespace our {
+    // Reads added value from the given json object
+    void ObstacleComponent::deserialize(const nlohmann::json& data){
+        if(!data.is_object()) return;
+        subtractedValue = data.value("subtractedValue", subtractedValue);
+    }
+}
---
+++ source/common/components/obstacle.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../ecs/component.hpp"
+
+#include <glm/glm.hpp>
+
+namespace our {
+
+    // This class handles data storage of fuel
+    class ObstacleComponent : public Component {
+    public:
+        int subtractedValue = 10;
+
+        // The ID of this component type is "Obstacle"
+        static std::string getID() { return "Obstacle"; }
+
+        // Reads subtracted value from the given json object
+        void deserialize(const nlohmann::json& data) override;
+    };
+
+}
