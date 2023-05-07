dbed0acbc1d34d7576c531267c551f27926f4322
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2023-05-07 22:25:03 +0200

    finished fuel component

---
+++ source/common/components/fuel.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../ecs/component.hpp"
+
+#include <glm/glm.hpp>
+
+namespace our {
+
+    // This class handles data storage of fuel
+    class FuelComponent : public Component {
+    public:
+        int addedValue = 10;
+
+        // The ID of this component type is "Fuel"
+        static std::string getID() { return "Fuel"; }
+
+        // Reads added value from the given json object
+        void deserialize(const nlohmann::json& data) override;
+    };
+
+}
